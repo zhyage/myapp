86f3ffc339e7ae867bcf36815214fb59236b180a
zhy <zhyage@yeah.net>
2015-07-10 11:13:53 +0800

    ok to dynamic add checkbox

--- public/javascripts/biaoZhunHua.js
+++ public/javascripts/biaoZhunHua.js
@@ -36,13 +36,13 @@ $(document).ready(function() {
                 "methodList": {
                     "type": "string",
                     "title": "methodList",
-                    "required" : true,
+                    "required": true,
                     "enum": buttonNameList
                 }
             },
             "form": [{
                 "key": "methodList",
-                "onChange": function (evt) {
+                "onChange": function(evt) {
                     var value = $(evt.target).val();
                     if (value) alert(value);
                 }
@@ -52,7 +52,7 @@ $(document).ready(function() {
 
     }
 
-    var biaoZhunHuaMatrix = new matrix(); 
+    var biaoZhunHuaMatrix = new matrix();
     var colNameList = [];
 
     function getMatrixData() {
@@ -71,37 +71,61 @@ $(document).ready(function() {
 
     }
 
-    function showMatrixColNameList() {
+
+    function generaterColumList() {
 
         getMatrixData();
+        var appendStr = "";
+        var i = 0;
+        for (i = 0; i < colNameList.length; i++) {
+            var colName = colNameList[i];
+            console.info("colName : ", colName);
+            var tmpStr = '<label><input type="checkbox" name= ' + colName + '  /> ' + colName + ' </label> <br>';
+            console.info("tmpStr : ", tmpStr);
+            appendStr = appendStr.concat(tmpStr);
 
-        $("#colNameList").jsonForm({
-            "schema": {
-                "menu": {
-                    "type": "array",
-                    "title": "Options",
-                    "items": {
-                        "type": "string",
-                        "title": "Option",
-                        "enum": colNameList
-                    }
-                }
-            },
-            "form": [{
-                "key": "menu",
-                "onClick": function (evt) {
-                    alert("checked");
-                    var value = $(evt.target).val();
-                    if (value) alert(value);
-                },
-                "type": "checkboxes",
+        }
+        console.log("appendStr :", appendStr);
+        $("#colNameList").append(appendStr);
+        attachCheckboxHandlers();
 
-                
-            }]
-        })
     }
 
+    // call onload or in script segment below form
+    function attachCheckboxHandlers() {
+        // get reference to element containing toppings checkboxes
+        var el = document.getElementById('colNameList');
+
+        // get reference to input elements in toppings container element
+        var tops = el.getElementsByTagName('input');
+
+        // assign updateTotal function to onclick property of each checkbox
+        for (var i = 0, len = tops.length; i < len; i++) {
+            if (tops[i].type === 'checkbox') {
+                tops[i].onclick = columDataSelected;
+            }
+        }
+    }
+
+    // called onclick of toppings checkboxes
+    function columDataSelected(e) {
+
+
+        // if check box is checked, add its value to val, otherwise subtract it
+        if (this.checked) {
+            alert(this.name + "checked");
+        } else {
+            alert(this.name + "unchecked");
+        }
+
+    }
+
+
+
+
     getFormulaConfFromServer();
-    showMatrixColNameList();
+    generaterColumList();
+    attachCheckboxHandlers();
+
 
 })
